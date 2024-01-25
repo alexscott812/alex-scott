9c7f4b9313e8bcfe5156468e68c9f2c716033d9d
alexscott812 <alexscott812@gmail.com>
2024-01-24 20:58:13 -0500

    active button style changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 
 const getColorClasses = (color: string): string => {
   if (color === 'secondary') {
-    return 'text-black dark:text-gray-100 bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800 focus:ring-gray-100 dark:focus:ring-gray-900 focus:ring-opacity-30 dark:focus:ring-opacity-30';
+    return 'text-black dark:text-gray-100 bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800 focus:ring-gray-500 dark:focus:ring-gray-400 focus:ring-opacity-30 dark:focus:ring-opacity-30';
   }
   return 'text-white dark:text-white bg-violet-600 dark:bg-violet-500 hover:bg-violet-700 dark:hover:bg-violet-400 focus:ring-violet-600 dark:focus:ring-violet-500 focus:ring-opacity-30 dark:focus:ring-opacity-30';
 };
